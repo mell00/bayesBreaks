d1dd7f37035dc507364864918b181053bcf7087c
Madison Ell <mellmell2550@gmail.com>
2025-12-16 16:03:46 -0600

    Register package code

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,7 @@
 #' Package initialization
 #'
 #' Registers package's compiled code. imports helpers needed for routine usage.
+#' @name bayesBreaks-internal
 #' @useDynLib bayesBreaks, .registration = TRUE
 #' @importFrom Rcpp sourceCpp
 NULL

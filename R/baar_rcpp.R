d67b5e5da8bbee403ca98242ad2d0372d9c54f98
Madison Ell <mellmell2550@gmail.com>
2025-12-16 19:48:39 -0600

    Wrap compiled outputs in data frames with breakpoint column names

--- R/baar_rcpp.R
+++ R/baar_rcpp.R
@@ -9,8 +9,22 @@
 baar_rcpp <- function(k = NULL, time, data, iterations, burn_in = 50,
                       make_murder_p = 0.5, percent = 0.02, lambda = 1, jump_p = 0.25,
                       ar = 1, progress = TRUE, fit_storage = FALSE) {
-  .Call("rcpp_baar", PACKAGE = "bayesBreaks", k, time, data, as.integer(iterations),
-        as.integer(burn_in), as.numeric(make_murder_p), as.numeric(percent),
-        as.numeric(lambda), as.numeric(jump_p), as.integer(ar), as.logical(progress),
-        as.logical(fit_storage))
+  out <- .Call("rcpp_baar", PACKAGE = "bayesBreaks", k, time, data, as.integer(iterations),
+               as.integer(burn_in), as.numeric(make_murder_p), as.numeric(percent),
+               as.numeric(lambda), as.numeric(jump_p), as.integer(ar), as.logical(progress),
+               as.logical(fit_storage))
+
+  # similar to R implementation w/ data.frame outputs and numeric BIC column
+  out$BIC <- data.frame(BIC = as.numeric(out$BIC))
+
+  bp_mat <- as.matrix(out$Breakpoints)
+  if (ncol(bp_mat) == 0) {
+    out$Breakpoints <- data.frame(matrix(nrow = iterations, ncol = 0))
+  } else {
+    colnames(bp_mat) <- as.character(seq_len(ncol(bp_mat)))
+    out$Breakpoints <- as.data.frame(bp_mat)
+  }
+
+  out
 }
+

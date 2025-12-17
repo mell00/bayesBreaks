c5c0b65329ce1b499366593a1388c3bf14365d1e
Madison Ell <mellmell2550@gmail.com>
2025-12-16 20:05:11 -0600

    Add safety fallback when compiled search returns empty breakpoint sets

--- R/bai_perron_rcpp.R
+++ R/bai_perron_rcpp.R
@@ -18,6 +18,7 @@ bai_perron_ar_rcpp <- function(data, order = 1L, interval = 0.15, max_breaks = 3
   bp <- res$breakpoints
   all_bp <- res$all_breakpoints
   if ((max_breaks > 0 && length(bp) > 0 && all(bp == 0)) ||
+      length(bp) == 0 ||
       (max_breaks > 0 && length(all_bp) > 0 && all(vapply(all_bp, length, integer(1)) == 0))) {
     return(bai_perron_ar(data, order = order, interval = interval, max_breaks = max_breaks, progress = FALSE))
   }

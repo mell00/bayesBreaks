b73f082d472351060f7031027fb6a2260f85c2f1
Madison Ell <mellmell2550@gmail.com>
2025-12-16 19:24:07 -0600

    Fix data documentation unknown value bug

--- R/data_documentation.R
+++ R/data_documentation.R
@@ -8,6 +8,7 @@
 #' indices and a second column named after the dataset (for example
 #' `data_0_a`).
 #'
+#' @name data_0_a
 #' @format A data frame with two columns:
 #' \describe{
 #'   \item{time}{Integer sequence denoting the observation index.}
@@ -19,55 +20,72 @@
 #' data_8 data_9 data_10 data_11 data_44 data_100 data_200 data_300
 #' @docType data
 #' @keywords datasets
+#' @usage data(data_0_a)
+#' @usage data(data_0_b)
+#' @usage data(data_1)
+#' @usage data(data_2)
+#' @usage data(data_3)
+#' @usage data(data_4)
+#' @usage data(data_5)
+#' @usage data(data_6)
+#' @usage data(data_7)
+#' @usage data(data_8)
+#' @usage data(data_9)
+#' @usage data(data_10)
+#' @usage data(data_11)
+#' @usage data(data_44)
+#' @usage data(data_100)
+#' @usage data(data_200)
+#' @usage data(data_300)
 #' @examples
 #' data(data_0_a)
 #' head(data_0_a)
-"data_0_a"
+data_0_a <- NULL
 
 #' @rdname data_0_a
-"data_0_b"
+data_0_b <- NULL
 
 #' @rdname data_0_a
-"data_1"
+data_1 <- NULL
 
 #' @rdname data_0_a
-"data_2"
+data_2 <- NULL
 
 #' @rdname data_0_a
-"data_3"
+data_3 <- NULL
 
 #' @rdname data_0_a
-"data_4"
+data_4 <- NULL
 
 #' @rdname data_0_a
-"data_5"
+data_5 <- NULL
 
 #' @rdname data_0_a
-"data_6"
+data_6 <- NULL
 
 #' @rdname data_0_a
-"data_7"
+data_7 <- NULL
 
 #' @rdname data_0_a
-"data_8"
+data_8 <- NULL
 
 #' @rdname data_0_a
-"data_9"
+data_9 <- NULL
 
 #' @rdname data_0_a
-"data_10"
+data_10 <- NULL
 
 #' @rdname data_0_a
-"data_11"
+data_11 <- NULL
 
 #' @rdname data_0_a
-"data_44"
+data_44 <- NULL
 
 #' @rdname data_0_a
-"data_100"
+data_100 <- NULL
 
 #' @rdname data_0_a
-"data_200"
+data_200 <- NULL
 
 #' @rdname data_0_a
-"data_300"
+data_300 <- NULL

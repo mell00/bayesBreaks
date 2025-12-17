6879b3cbcfbb53f481e22aaaf4a803c4ccfae7b0
Madison Ell <mellmell2550@gmail.com>
2025-12-16 19:49:47 -0600

    Output to plain vectors and restored ts attributes when the input carries a time base

--- R/fit_ar_rcpp.R
+++ R/fit_ar_rcpp.R
@@ -6,5 +6,19 @@
 #' @inheritParams FitAR
 #' @export
 FitAR_rcpp <- function(z, p, demean = TRUE) {
-  .Call("rcpp_fit_ar", PACKAGE = "bayesBreaks", z, as.integer(p), as.logical(demean))
+  res <- .Call("rcpp_fit_ar", PACKAGE = "bayesBreaks", z, as.integer(p), as.logical(demean))
+
+  # drop any matrix dimensions introduced by Armadillo and restore ts attributes
+  res$phiHat <- as.numeric(res$phiHat)
+  res$res <- as.numeric(res$res)
+  res$fits <- as.numeric(res$fits)
+
+  tsp_in <- stats::tsp(z)
+  if (!is.null(tsp_in)) {
+    res$res <- stats::ts(res$res, start = tsp_in[1], frequency = tsp_in[3])
+    res$fits <- stats::ts(res$fits, start = tsp_in[1], frequency = tsp_in[3])
+  }
+
+  res
 }
+

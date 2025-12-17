6ac6574f36a88b09e15a870e97c00e540132a816
Madison Ell <mellmell2550@gmail.com>
2025-12-16 19:49:16 -0600

    Name SSR/BIC entries and fall back to the R helper when the compiled search yields degenerate breakpoint sets

--- R/bai_perron_rcpp.R
+++ R/bai_perron_rcpp.R
@@ -9,6 +9,18 @@
 #'   matching the native R implementation.
 #' @export
 bai_perron_ar_rcpp <- function(data, order = 1L, interval = 0.15, max_breaks = 3L) {
-  .Call("rcpp_bai_perron", PACKAGE = "bayesBreaks", data, as.integer(order),
-        as.numeric(interval), as.integer(max_breaks))
+  res <- .Call("rcpp_bai_perron", PACKAGE = "bayesBreaks", data, as.integer(order),
+               as.numeric(interval), as.integer(max_breaks))
+
+  # normalise names and fall back to the R helper if the compiled result is degenerate
+  names(res$SSR) <- names(res$BIC) <- as.character(0:max_breaks)
+
+  bp <- res$breakpoints
+  all_bp <- res$all_breakpoints
+  if ((max_breaks > 0 && length(bp) > 0 && all(bp == 0)) ||
+      (max_breaks > 0 && length(all_bp) > 0 && all(vapply(all_bp, length, integer(1)) == 0))) {
+    return(bai_perron_ar(data, order = order, interval = interval, max_breaks = max_breaks, progress = FALSE))
+  }
+
+  res
 }
